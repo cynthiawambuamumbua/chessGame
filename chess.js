662e50d1e025e15ca07c455227b26caf119ef8ed
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-04-23 20:28:08 +0300

    first commit

---
+++ chess.js
@@ -0,0 +1,94 @@
+function coloring(){
+    const color=document.querySelectorAll('.table')
+    color.forEach[color=>{
+        getClass=color.class
+        arr.shift()
+        aside=eval(arr.pop())
+        aup=eval(arr.shift())
+        i=aside+aup
+        
+    }]
+}
+
+
+for(i=1;i<9;i++){
+    let box1=document.createElement('div')
+    if(i%2==0){
+        box1.style.backgroundColor=' black'
+    }
+    else{
+        box1.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box2=document.createElement('div')
+    if(i%2==0){
+        box2.style.backgroundColor='white'
+    }
+    else{
+        box2.style.backgroundColor='black'
+    }
+}
+for(i=1;i<9;i++){
+    let box3=document.createElement('div')
+    if(i%2==0){
+        box3.style.backgroundColor='black'
+    }
+    else{
+        box23.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box4=document.createElement('div')
+    if(i%2==0){
+        box4.style.backgroundColor='black'
+    }
+    else{
+        box4.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box5=document.createElement('div')
+    if(i%2==0){
+        box5.style.backgroundColor='black'
+    }
+    else{
+        box5.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box6=document.createElement('div')
+    if(i%2==0){
+        box6.style.backgroundColor='black'
+    }
+    else{
+        box6.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box7=document.createElement('div')
+    if(i%2==0){
+        box7.style.backgroundColor='black'
+    }
+    else{
+        box7.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box8=document.createElement('div')
+    if(i%2==0){
+        box8.style.backgroundColor='black'
+    }
+    else{
+        box8.style.backgroundColor='white'
+    }
+}
+for(i=1;i<9;i++){
+    let box9=document.createElement('div')
+    if(i%2==0){
+        box9.style.backgroundColor='black'
+    }
+    else{
+        box9.style.backgroundColor='white'
+    }
+}
